b7bae43f566db77e8cfd32f5b5bae550c8d7630a
Wilfredo Catalan <catalanwilfredo97@gmail.com>
2025-04-20 00:44:33 +0800

    update the server logic in uploading an images

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 // Set up multer for file uploads
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'uploads/');
+        cb(null, 'images/');
     },
     filename: (req, file, cb) => {
         cb(null, Date.now() + path.extname(file.originalname));
